7e64cfcabe940b69635853b7ffe2492fdbeeaa11
Jencir Lee <jli05@icloud.com>
2016-04-25 23:38:39 +0100

    split out DataCumulant case class

--- src/main/scala/edu/uci/eecs/spectralLDA/datamoments/DataCumulant.scala
+++ src/main/scala/edu/uci/eecs/spectralLDA/datamoments/DataCumulant.scala
@@ -14,82 +14,133 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{Accumulator, SparkContext}
 import scala.collection.mutable
 
-class DataCumulant(sc: SparkContext, dimK: Int, alpha0: Double, tolerance: Double, documents:RDD[(Long, Double, SparseVector[Double])],dimVocab: Int,numDocs: Long ) extends Serializable {
+import scalaxy.loops._
+import scala.language.postfixOps
 
-  private val M1: Accumulator[DenseVector[Double]] = sc.accumulator(breeze.linalg.DenseVector.zeros[Double](dimVocab))(DenseVectorAccumulatorParam)
-  println("Start calculating first order moments...")
-  documents.foreach { case (_, length, vec) => M1 += update_firstOrderMoments(dimVocab, vec.toDenseVector, length) }
-  private val firstOrderMoments: DenseVector[Double] = M1.value.map(x => x / numDocs.toDouble)
-  println("Finished calculating first order moments.")
-
-  val (thirdOrderMoments: DenseMatrix[Double], unwhiteningMatrix: DenseMatrix[Double]) = {
-    import scalaxy.loops._
-    import scala.language.postfixOps
+case class DataCumulant(thirdOrderMoments: DenseMatrix[Double], unwhiteningMatrix: DenseMatrix[Double])
+  extends Serializable
 
 
+object DataCumulant {
+  def getDataCumulant(sc: SparkContext,
+                      dimK: Int, alpha0: Double, tolerance: Double,
+                      documents: RDD[(Long, Double, SparseVector[Double])],
+                      dimVocab: Int,
+                      numDocs: Long ): DataCumulant = {
+    println("Start calculating first order moments...")
+    val M1: DenseVector[Double] = documents map {
+      case (_, length, vec) => update_firstOrderMoments(dimVocab, vec.toDenseVector, length)
+    } reduce (_ + _)
+
+    val firstOrderMoments: DenseVector[Double] = M1 / numDocs.toDouble
+    println("Finished calculating first order moments.")
+
+    val (thirdOrderMoments: DenseMatrix[Double], unwhiteningMatrix: DenseMatrix[Double]) = computeThirdOrderMoments(
+      sc, alpha0, dimVocab, dimK,
+      numDocs, firstOrderMoments, documents,
+      tolerance
+    )
+
+    new DataCumulant(thirdOrderMoments, unwhiteningMatrix)
+  }
+
+  private def computeThirdOrderMoments(sc: SparkContext,
+                                       alpha0: Double,
+                                       dimVocab: Int, dimK: Int,
+                                       numDocs: Long,
+                                       firstOrderMoments: DenseVector[Double],
+                                       documents: RDD[(Long, Double, SparseVector[Double])],
+                                       tolerance: Double)
+  : (DenseMatrix[Double], DenseMatrix[Double]) = {
     println("Start calculating second order moments...")
-    val (eigenVectors: DenseMatrix[Double], eigenValues: DenseVector[Double]) = whiten(sc, alpha0, dimVocab, dimK, numDocs, firstOrderMoments, documents)
+    val (eigenVectors: DenseMatrix[Double], eigenValues: DenseVector[Double]) = whiten(sc, alpha0,
+      dimVocab, dimK, numDocs, firstOrderMoments, documents)
     println("Finished calculating second order moments and whitening matrix.")
 
     println("Start whitening data with dimensionality reduction...")
-    val whitenedData: RDD[(breeze.linalg.DenseVector[Double], Double)] = documents.map {
-      case (_, length, vec) => (project(dimVocab, dimK, alpha0, eigenValues, eigenVectors, vec), length)
+    val whitenedData: RDD[(DenseVector[Double], Double)] = documents.map {
+      case (_, length, vec) => (project(dimVocab, dimK, alpha0, eigenValues, eigenVectors, vec)(tolerance), length)
     }
-    val firstOrderMoments_whitened: DenseVector[Double] = whitenedData.map(x => x._1 / x._2).reduce((a, b) => a :+ b).map(x => x / numDocs.toDouble)
+    val firstOrderMoments_whitened: DenseVector[Double] = whitenedData
+      .map(x => x._1 / x._2)
+      .reduce((a, b) => a :+ b)
+      .map(x => x / numDocs.toDouble)
     println("Finished whitening data.")
 
     println("Start calculating third order moments...")
-    var Ta: Accumulator[DenseMatrix[Double]] = sc.accumulator(breeze.linalg.DenseMatrix.zeros[Double](dimK, dimK * dimK))(DenseMatrixAccumulatorParam)
-    whitenedData.foreach{ case (vec, len) => Ta += update_thirdOrderMoments(dimK, alpha0, firstOrderMoments_whitened, vec, len)}
+    var Ta: DenseMatrix[Double] = whitenedData map {
+      case (vec, len) => update_thirdOrderMoments(
+        dimK, alpha0,
+        firstOrderMoments_whitened,
+        vec, len)
+    } reduce(_ + _)
 
     val alpha0sq: Double = alpha0 * alpha0
     val Ta_shift = DenseMatrix.zeros[Double](dimK, dimK * dimK)
     for (id_i <- 0 until dimK optimized) {
       for (id_j <- 0 until dimK optimized) {
         for (id_l <- 0 until dimK optimized) {
-          Ta_shift(id_i, id_j * dimK + id_l) += alpha0sq * firstOrderMoments_whitened(id_i) * firstOrderMoments_whitened(id_j) * firstOrderMoments_whitened(id_l)
+          Ta_shift(id_i, id_j * dimK + id_l) += (alpha0sq * firstOrderMoments_whitened(id_i)
+              * firstOrderMoments_whitened(id_j) * firstOrderMoments_whitened(id_l))
         }
       }
     }
     println("Finished calculating third order moments.")
     val unwhiteningMatrix: breeze.linalg.DenseMatrix[Double] = eigenVectors * breeze.linalg.diag(eigenValues.map(x => scala.math.sqrt(x)))
-    (Ta.value.map(x => x / numDocs.toDouble) - Ta_shift, unwhiteningMatrix)
+    (Ta / numDocs.toDouble - Ta_shift, unwhiteningMatrix)
   }
 
-  private def whiten(sc: SparkContext, alpha0: Double, vocabSize: Int, dimK: Int, numDocs: Long, firstOrderMoments: breeze.linalg.DenseVector[Double], documents: RDD[(Long, Double, breeze.linalg.SparseVector[Double])]): (breeze.linalg.DenseMatrix[Double], breeze.linalg.DenseVector[Double]) = {
+  private def whiten(sc: SparkContext,
+                     alpha0: Double,
+                     vocabSize: Int, dimK: Int,
+                     numDocs: Long,
+                     firstOrderMoments: DenseVector[Double],
+                     documents: RDD[(Long, Double, SparseVector[Double])])
+  : (DenseMatrix[Double], DenseVector[Double]) = {
     val para_main: Double = (alpha0 + 1.0) / numDocs.toDouble
     val para_shift: Double = alpha0
 
     val SEED_random: Long = System.currentTimeMillis
     val gaussianRandomMatrix: DenseMatrix[Double] = AlgebraUtil.gaussian(vocabSize, dimK * 2, SEED_random)
     val gaussianRandomMatrix_broadcasted: Broadcast[breeze.linalg.DenseMatrix[Double]] = sc.broadcast(gaussianRandomMatrix)
-
     val firstOrderMoments_broadcasted: Broadcast[breeze.linalg.DenseVector[Double]] = sc.broadcast(firstOrderMoments.toDenseVector)
-    // val documents_broadcasted: Broadcast[Array[(Long, Double, breeze.linalg.SparseVector[Double])]] = sc.broadcast(documents.collect())
-    val M2_a_S: Accumulator[breeze.linalg.DenseMatrix[Double]] = sc.accumulator(breeze.linalg.DenseMatrix.zeros[Double](vocabSize, dimK * 2), "Second Order Moment multiplied with S: M2_a * S")(DenseMatrixAccumulatorParam)
-    documents.foreach(this_document => M2_a_S += accumulate_M_mul_S(vocabSize, dimK * 2, alpha0, firstOrderMoments_broadcasted.value, gaussianRandomMatrix_broadcasted.value, this_document._3, this_document._2))
 
-    M2_a_S.value *= para_main
+    val M2_a_S: DenseMatrix[Double] = documents map {
+      this_document => accumulate_M_mul_S(
+        vocabSize, dimK * 2,
+        alpha0,
+        firstOrderMoments_broadcasted.value,
+        gaussianRandomMatrix_broadcasted.value,
+        this_document._3, this_document._2)
+    } reduce(_ + _)
+
+    M2_a_S :*= para_main
     val shiftedMatrix: breeze.linalg.DenseMatrix[Double] = firstOrderMoments * (firstOrderMoments.t * gaussianRandomMatrix)
-    M2_a_S.value -= shiftedMatrix :* para_shift
+    M2_a_S -= shiftedMatrix :* para_shift
 
-    val Q = AlgebraUtil.orthogonalizeMatCols(M2_a_S.value)
-    val M2_a_Q: Accumulator[DenseMatrix[Double]] = sc.accumulator(breeze.linalg.DenseMatrix.zeros[Double](vocabSize, dimK * 2), "Second Order Moment multiplied with S: M2_a * S")(DenseMatrixAccumulatorParam)
+    val Q = AlgebraUtil.orthogonalizeMatCols(M2_a_S)
 
-    documents.foreach(this_document => M2_a_Q += accumulate_M_mul_S(vocabSize, dimK * 2, alpha0, firstOrderMoments, Q, this_document._3, this_document._2))
-    M2_a_Q.value *= para_main
+    val M2_a_Q: DenseMatrix[Double] = documents map {
+      this_document => accumulate_M_mul_S(
+        vocabSize,
+        dimK * 2, alpha0,
+        firstOrderMoments_broadcasted.value,
+        Q,
+        this_document._3, this_document._2)
+    } reduce(_ + _)
+    M2_a_Q :*= para_main
     val shiftedMatrix2: breeze.linalg.DenseMatrix[Double] = firstOrderMoments * (firstOrderMoments.t * Q)
-    M2_a_Q.value -= shiftedMatrix2 :* para_shift
+    M2_a_Q -= shiftedMatrix2 :* para_shift
 
     // Note: eigenvectors * Diag(eigenvalues) = M2_a_Q
-    val svd.SVD(u: breeze.linalg.DenseMatrix[Double], s: breeze.linalg.DenseVector[Double], v: breeze.linalg.DenseMatrix[Double]) = svd((M2_a_Q.value.t * M2_a_Q.value))
-    val eigenVectors: DenseMatrix[Double] = (M2_a_Q.value * u) * breeze.linalg.diag(s.map(entry => 1.0 / math.sqrt(entry)))
+    val svd.SVD(u: breeze.linalg.DenseMatrix[Double], s: breeze.linalg.DenseVector[Double], v: breeze.linalg.DenseMatrix[Double]) = svd(M2_a_Q.t * M2_a_Q)
+    val eigenVectors: DenseMatrix[Double] = (M2_a_Q * u) * breeze.linalg.diag(s.map(entry => 1.0 / math.sqrt(entry)))
     val eigenValues: DenseVector[Double] = s.map(entry => math.sqrt(entry))
     (eigenVectors(::, 0 until dimK), eigenValues(0 until dimK))
   }
 
   private def update_firstOrderMoments(dim: Int, Wc: breeze.linalg.DenseVector[Double], len: Double) = {
-    val M1: DenseVector[Double] = Wc.map(x => x/len)
+    val M1: DenseVector[Double] = Wc / len
     M1
   }
 
@@ -158,8 +209,11 @@ class DataCumulant(sc: SparkContext, dimK: Int, alpha0: Double, tolerance: Doubl
   }
 
   private def project(dimVocab: Int, dimK: Int, alpha0: Double,
-              eigenValues: breeze.linalg.DenseVector[Double], eigenVectors: breeze.linalg.DenseMatrix[Double],
-              Wc: breeze.linalg.SparseVector[Double]): breeze.linalg.DenseVector[Double] = {
+                      eigenValues: breeze.linalg.DenseVector[Double],
+                      eigenVectors: breeze.linalg.DenseMatrix[Double],
+                      Wc: breeze.linalg.SparseVector[Double])
+                     (implicit tolerance: Double)
+  : breeze.linalg.DenseVector[Double] = {
     var offset = 0
     val result = breeze.linalg.DenseVector.zeros[Double](dimK)
     while (offset < Wc.activeSize) {
